0a9f31ed33275afe45b8e441b5febbcf1e163f12
Sergio Covarrubias <75351438+secova25@users.noreply.github.com>
2021-04-24 23:21:24 -0500

    Create regpatients.sql

---
+++ Added SQL/sqls/regpatients.sql
@@ -0,0 +1,1 @@
+INSERT INTO patient(Fname,Middle,Lname, ssn,age,gender,race,occupation_class, Medical_history,phonenumber,address) values(Fname,Middle,Lname, ssn,age,gender,race,occupation_class, Medical_history,phonenumber,address);
